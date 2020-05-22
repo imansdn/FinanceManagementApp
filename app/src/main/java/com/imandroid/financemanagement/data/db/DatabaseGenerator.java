4eafeac9f4da618cfa959ad4660c7710e796c49a
Imansdn <imansadrian@gmail.com>
2020-05-23 01:27:31 +0430

    create database generator to make an instance of db in a singleton pattern

---
+++ app/src/main/java/com/imandroid/financemanagement/data/db/DatabaseGenerator.java
@@ -0,0 +1,42 @@
+package com.imandroid.financemanagement.data.db;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+
+
+@Database(entities = {ExpenditureEntity.class}, version = DatabaseGenerator.DATABASE_VERSION, exportSchema = false)
+public abstract class DatabaseGenerator extends RoomDatabase {
+    static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "finance_db";
+
+    private  static volatile DatabaseGenerator INSTANCE = null;
+    private static final Object lock = new Object();
+
+    public abstract ExpenditureDao expenditureDao();
+
+    public static DatabaseGenerator getInstance(Context context) {
+
+        DatabaseGenerator instance = INSTANCE;
+
+        synchronized(lock) {
+            if (INSTANCE == null){
+                instance = Room.databaseBuilder(context,DatabaseGenerator.class,DATABASE_NAME)
+                        .fallbackToDestructiveMigration()
+                        .build();
+
+                INSTANCE = instance;
+            }
+
+
+        }
+        return instance;
+    }
+
+
+
+
+}

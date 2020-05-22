2733c9fa426df07b29c4184b14d8490adde137ac
Imansdn <imansadrian@gmail.com>
2020-05-23 00:42:15 +0430

    create application class and add it to manifest

---
+++ app/src/main/java/com/imandroid/financemanagement/FinanceManagement.java
@@ -0,0 +1,11 @@
+package com.imandroid.financemanagement;
+
+import android.app.Application;
+
+public class FinanceManagement extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}

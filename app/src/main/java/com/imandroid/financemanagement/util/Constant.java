c8e2421604592c3b1e5647024648a466cb73ee03
Imansdn <imansadrian@gmail.com>
2020-05-23 04:59:18 +0430

    define an enum for expenditure categories

--- app/src/main/java/com/imandroid/financemanagement/util/Constant.java
+++ app/src/main/java/com/imandroid/financemanagement/util/Constant.java
@@ -1,4 +1,13 @@
 package com.imandroid.financemanagement.util;
 
 public class Constant {
+
+    public enum EXPENDITURE_CATEGORIES {
+         Subscription
+        ,Entertainment
+        ,Food
+        ,Transport
+        ,Others}
+
+
 }

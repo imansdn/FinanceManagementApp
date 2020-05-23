542cc51fea301020c162d177ab6044aaf60a4897
Imansdn <imansadrian@gmail.com>
2020-05-23 04:57:29 +0430

    create fragment ,viewmodel and xml for report list

---
+++ app/src/main/java/com/imandroid/financemanagement/screen/reportList/ReportListFragment.java
@@ -0,0 +1,38 @@
+package com.imandroid.financemanagement.screen.reportList;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.imandroid.financemanagement.R;
+
+public class ReportListFragment extends Fragment {
+
+    private ReportListViewModel mViewModel;
+
+    public static ReportListFragment newInstance() {
+        return new ReportListFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.report_list_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(ReportListViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/imandroid/financemanagement/screen/reportList/ReportListViewModel.java
@@ -0,0 +1,7 @@
+package com.imandroid.financemanagement.screen.reportList;
+
+import androidx.lifecycle.ViewModel;
+
+public class ReportListViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}

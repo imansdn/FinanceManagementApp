c39b243f4a4473fa1a70f182f5a4bd8f4ed0cf91
Imansdn <imansadrian@gmail.com>
2020-05-24 10:08:14 +0430

    create a dialog for changes the expenses

---
+++ app/src/main/java/com/imandroid/financemanagement/screen/dashboard/BudgetDialog.java
@@ -0,0 +1,78 @@
+package com.imandroid.financemanagement.screen.dashboard;
+
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.view.LayoutInflater;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
+
+import com.imandroid.financemanagement.R;
+import com.imandroid.financemanagement.data.GeneralRepository;
+import com.imandroid.financemanagement.util.Constant;
+
+
+public class BudgetDialog {
+    private GeneralRepository repository;
+    private Activity activity;
+
+    public BudgetDialog(GeneralRepository repository, Activity activity) {
+        this.repository = repository;
+        this.activity = activity;
+    }
+
+    public void buildDialog() {
+        final Activity thisActivity = activity;
+        AlertDialog.Builder builder = new AlertDialog.Builder(thisActivity, R.style.AlertDialogTheme);
+        // Get the layout inflater
+        LayoutInflater inflater = thisActivity.getLayoutInflater();
+
+        final LinearLayout layout = (LinearLayout) inflater.inflate(R.layout.budget_popup, null);
+        builder.setView(layout);
+
+        EditText daily = layout.findViewById(R.id.daily_budget);
+        EditText weekly = layout.findViewById(R.id.weekly_budget);
+        EditText monthly = layout.findViewById(R.id.monthly_budget);
+
+        daily.setText(String.valueOf(repository.getBudget(Constant.TIME_PERIOD.DAILY.name())));
+        weekly.setText(String.valueOf(repository.getBudget(Constant.TIME_PERIOD.WEEKLY.name())));
+        monthly.setText(String.valueOf(repository.getBudget(Constant.TIME_PERIOD.MONTHLY.name())));
+
+        // Inflate and set the layout for the dialog
+        // Pass null as the parent view because its going in the dialog layout
+        // Add action buttons
+        builder.setPositiveButton(R.string.confirm, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+                try {
+
+                    EditText edited_daily = layout.findViewById(R.id.daily_budget);
+                    EditText edited_weekly = layout.findViewById(R.id.weekly_budget);
+                    EditText edited_monthly = layout.findViewById(R.id.monthly_budget);
+                    repository.setBudget(Constant.TIME_PERIOD.DAILY.name(),Integer.parseInt(edited_daily.getText().toString()));
+                    repository.setBudget(Constant.TIME_PERIOD.WEEKLY.name(),Integer.parseInt(edited_weekly.getText().toString()));
+                    repository.setBudget(Constant.TIME_PERIOD.MONTHLY.name(),Integer.parseInt(edited_monthly.getText().toString()));
+                    Toast.makeText(activity, R.string.budget_changes, Toast.LENGTH_SHORT).show();
+
+                    dialog.dismiss();
+//                    activity.finish();
+//                    activity.startActivity(activity.getIntent());
+                } catch (NumberFormatException e) {
+                    Toast.makeText(activity, R.string.the_budget_must_be_filled, Toast.LENGTH_SHORT).show();
+                }
+            }
+        })
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+        //dialog.getWindow().setLayout(1200, 1800);
+    }
+}
